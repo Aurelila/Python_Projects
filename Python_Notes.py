cccf43a6f981c41a22edc8ba150fdaf9043f7a85
Aurelila <nmccoy176@gmail.com>
2020-05-09 23:50:07 -0400

    2

---
+++ Python_Notes.py
@@ -0,0 +1,30 @@
+my_name = "Codecademy"
+print("Hello and welcome " + my_name + "!")
+
+# This is a comment!
+print("Hello everyone!")
+
+#Print your name
+print("Nancy McCoy")
+
+# Greeting
+message_string = "Hello there"
+print(message_string)
+
+# Farewell
+message_string = "Hasta la vista"
+print(message_string)
+
+#Concatentation with strings; The + operator adds strings together or otherwise known as string concatenation.
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+# Define message below:
+message = string1 + string2 + string3 + string4 + string5 + string6
+
+#print(message)
+print(message)
