0fab300a2af4369fb38e0e667cbfc0373a1d2228
Aurelila <nmccoy176@gmail.com>
2020-05-09 23:56:56 -0400

    Add existing file

--- Python_Notes.py
+++ Python_Notes.py
@@ -28,3 +28,14 @@ message = string1 + string2 + string3 + string4 + string5 + string6
 
 #print(message)
 print(message)
+
+
+print('Hello, world!')
+print ('What is your name?') #asks for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?') # asks for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + 'in a year.')
